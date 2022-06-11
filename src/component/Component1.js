9690cbc0dd974438dffdb23c92cb84ab048ed10e
Sippu <sippu@Simpys-MacBook-Air.local>
2022-06-11 15:00:26 +0530

    overcome of className overid problem

---
+++ src/component/Component1.js
@@ -0,0 +1,6 @@
+import styles from './Component1.module.css';
+
+const Component1 =()=>{
+    return <button className= {styles.buttonColor}>Component1 Button</button>
+}
+export default Component1;
---
+++ src/component/Component2.js
@@ -0,0 +1,6 @@
+import style from './Component2.module.css';
+
+const Component2 =()=>{
+    return <button className={style.buttonColor}>Component2 Button</button>
+}
+export default Component2;
